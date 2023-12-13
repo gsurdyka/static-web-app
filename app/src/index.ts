f19f700a15f1382e91114191ecf72dfe279f8390
gsurdyka <grzegorz.surdyka@cytiva.com>
2023-12-13 16:26:54 +0100

    add func button test

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,12 +3,22 @@ import './app.css';
 function main(): void {
     const button = document.querySelector("#testButton");
     button?.addEventListener("click", testSubmitHandler);
+    const fButton = document.querySelector("#funcButton");
+    fButton?.addEventListener("click", testSubmitFuncHandler);
 }
 
 function testSubmitHandler(e: Event): void {
     e.preventDefault();
     const outputElement = document.querySelector('#output');
     outputElement.textContent = 'Scripts work!';
+}
+
+async function testSubmitFuncHandler(e: Event): Promise<void>{
+    e.preventDefault();
+    const apiResponse = await fetch('/api/Example');
+    const apiResponseText = await apiResponse.text();
+    const outputElement = document.querySelector('#output');
+    outputElement.textContent = apiResponseText;
 }
 
 main();
